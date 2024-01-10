57267d2becdd7a0871a8480e8c23e41a56a6139e
deepu024 <ojashsharma024@gmail.com>
2024-01-10 14:01:45 +0530

    added header and theme toggler

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import { RedirectToSignIn, SignIn, SignInButton, SignedOut, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { ThemeToggler } from './ThemeToggler'
+
+const Header = () => {
+    return (
+        <header className='flex items-center justify-between border-b-[1px]'>
+            <Link href={'/'}>
+                <div className='flex items-center gap-3'>
+                    <Image
+                        src={"/drop-box-logo.png"}
+                        alt='Drop Box'
+                        width={50}
+                        height={50}
+                    />
+                    <p className='text-xl font-bold'>DropBox</p>
+                </div>
+            </Link>
+
+            <div className='px-5 flex items-center gap-2'>
+                <ThemeToggler />
+                <UserButton afterSignOutUrl='/' />
+                <SignedOut>
+                    <SignInButton afterSignInUrl='/dashboard' />
+                </SignedOut>
+            </div>
+        </header>
+    )
+}
+
+export default Header
